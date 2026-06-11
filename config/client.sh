a6524f5cc5dd65b1d315ebdccdc2c9b4186fab5c
picetor <piceters@gmail.com>
2026-06-11 23:38:01 +0800

    update

--- config/client.sh
+++ config/client.sh
@@ -6,7 +6,14 @@ chmod +x ppp ppp.sh
 tmux kill-session -t ppp 2>/dev/null
 
 # 创建新的后台 tmux 会话（提供伪终端，ppp 可显示 TUI）
-tmux new-session -d -s ppp './ppp --mode=client --config=./{config}.json --tun-mux=0 --dns=127.0.0.1 --tun-host=yes --tun-vnet=yes --tun-gw=192.168.12.0 --tun-ip=192.168.12.10 --tun-flash=yes --tun-mask=24 --link-restart=3 --tun-static=no --block-quic=yes --tun-mux-acceleration=3 --tun-ssmt=4/mq'
+tmux new-session -d -s ppp '
+
+ulimit -SHn 1000000
+ulimit -c unlimited
+
+iptables -t nat -F
+iptables -t nat -A POSTROUTING -j MASQUERADE
+./ppp --mode=client --config=./{config}.json --tun-mux=0 --dns=127.0.0.1 --tun-host=yes --tun-vnet=yes --tun-gw=192.168.12.0 --tun-ip=192.168.12.10 --tun-flash=yes --tun-mask=24 --link-restart=3 --tun-static=no --block-quic=yes --tun-mux-acceleration=3 --tun-ssmt=4/mq'
 
 # 等待 tmux 会话完全创建
 sleep 1
