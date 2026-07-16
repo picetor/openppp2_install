9621d4e738cf221deaab8d9df02e694a6c583ddc
picetor <91052711+picetor@users.noreply.github.com>
2026-07-16 21:27:06 +0800

    Update start.bat

--- config/start.bat
+++ config/start.bat
@@ -6,4 +6,4 @@ if errorlevel 1 (
 )
 
 cd /d "%~dp0"
-start "openppp2" ppp.exe --mode=client --config=./config/zgo.json --tun-mux=0 --tun-host=yes --tun-vnet=yes --tun-gw=192.168.12.0 --tun-ip=192.168.12.68 --tun-flash=yes --tun-mask=24 --link-restart=3 --tun-static=no --tun-mux-acceleration=3 --bypass-mode=geo --block-quic=yes --log-file ./ppp1.log --lwip=yes 
+start "openppp2" ppp.exe --mode=client --config=./appsettings.json --tun-mux=0 --tun-host=yes --tun-vnet=yes --tun-gw=192.168.12.0 --tun-ip=192.168.12.68 --tun-flash=yes --tun-mask=24 --link-restart=3 --tun-static=no --tun-mux-acceleration=3 --bypass-mode=geo --block-quic=yes --lwip=yes 
